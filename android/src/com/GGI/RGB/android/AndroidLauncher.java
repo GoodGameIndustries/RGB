7545b7c89ade13889e48492539ffc15a2c34d508
EmmettGGI <goodgameindustries@gmail.com>
2014-08-07 05:14:54 -0400

    RGB Setup
    setup

---
+++ android/src/com/GGI/RGB/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.GGI.RGB.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.GGI.RGB.RGB;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RGB(), config);
+	}
+}
---
+++ desktop/src/com/GGI/RGB/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.GGI.RGB.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.GGI.RGB.RGB;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RGB(), config);
+	}
+}
