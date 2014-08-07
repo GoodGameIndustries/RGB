46104e84c9ec153cf945a69c279dc54b8fe2b752
StevenGGI <stevenliang699@yahoo.com>
2014-08-07 16:36:50 -0400

    successful target move

--- core/src/com/GGI/Components/Circle.java
+++ core/src/com/GGI/Components/Circle.java
@@ -23,9 +23,8 @@ public class Circle {
 	private Beacon wait3=new Beacon(0.45f,0.86f);
 	private Beacon wait2=new Beacon(0.45f,0.80f);
 	private Beacon wait1=new Beacon(0.45f,0.74f);
-	private Beacon target;
+	private Beacon target=wait4;
 	private int count=5;
-	
 	public Circle(){
 		t = randomColor();
 	}
@@ -35,16 +34,21 @@ public class Circle {
 		//x=target.getX();y=target.getY();
 	}
 	
+	private void setPosition(float x, float y){this.x=x;this.y=y;}
+	
 	private float calculateDistance(Beacon beacon){
 		return (float) Math.pow(Math.pow(beacon.getX()-x,2)+Math.pow(beacon.getY()-y,2),0.5);
 	}
 	
-	private void targetMove(Beacon beacon){
-		double r=calculateDistance(beacon);
+	
+	private void targetMove(){
+		double r=calculateDistance(target);
 		System.out.println(r);
-		x=(float) (x+Math.abs(beacon.getX()-x)/r);
-		y=(float) (y+Math.abs(beacon.getY()-y)/r);
-		if(r<50){return;}
+		if(r>50){
+			x=(float) (x+(target.getX()-x)/r);
+			y=(float) (y+(target.getY()-y)/r);
+		}
+		else{x=target.getX();y=target.getY();}
 		//moveBound();
 	}
 	
@@ -55,13 +59,14 @@ public class Circle {
 	
 	public void move(){
 		switch(count){
-			case 5:setTarget(wait4);targetMove(target);count--;
-			case 4:setTarget(wait3);targetMove(target);count--;
-			case 3:setTarget(wait2);targetMove(target);count--;
-			case 2:setTarget(wait1);targetMove(target);count--;
+			case 5:setPosition(wait4.getX(),wait4.getY());count--;
+			case 4:setPosition(wait3.getX(),wait3.getY());count--;
+			case 3:setPosition(wait2.getX(),wait2.getY());count--;
+			case 2:setPosition(wait1.getX(),wait1.getY());count--;
 			case 1://y=y-0.1f;
-				setTarget(red);targetMove(target);
-		}
+				setTarget(green);
+				targetMove();
+			}
 	}
 	
 	private Texture randomColor() {
